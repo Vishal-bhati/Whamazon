5c94248cb3ea51a4da3dc31450e76076b3dd4f35
Vishal-bhati <vishalbhati8368@gmail.com>
2024-04-22 01:08:33 +0530

    admin fix

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -16,7 +16,6 @@ urlpatterns = [
     path('view-feedback', views.view_feedback_view,name='view-feedback'),
 
     path('adminclick', views.adminclick_view),
-    path('adminlogin', LoginView.as_view(template_name='ecom/adminlogin.html'),name='adminlogin'),
     path('admin-dashboard', views.admin_dashboard_view,name='admin-dashboard'),
 
     path('view-customer', views.view_customer_view,name='view-customer'),
